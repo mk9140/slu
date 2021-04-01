56c4580154365c6cc579a9e966a7caa95b409051
mk9140 <31506088+mk9140@users.noreply.github.com>
2021-04-01 20:41:44 +0900

    init

---
+++ src/main/java/com/myhobby/slu/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.myhobby.slu.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@Log4j2 // 로그 프레임워크 사용 lombok의 어노테이션. 이외에도 여러 로깅 프레임워크 있음.
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String toTest() {
+        log.error("로그 테스트 : Log4j2의 에러 로그");
+        log.warn("로그 테스트 : Log4j2의 경고 로그");
+        log.info("로그 테스트 : Log4j2의 정보 로그 ");
+
+        return "test/index";
+
+    }
+}
---
+++ src/test/java/com/myhobby/slu/SluApplicationTests.java
@@ -0,0 +1,11 @@
+package com.myhobby.slu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SluApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
